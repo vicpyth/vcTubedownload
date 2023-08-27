c0726735c3dd70c52b7da9030ef761d76a7ac144
vicpyth <91341103+vicpyth@users.noreply.github.com>
2023-08-28 01:06:43 +0800

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,57 +1,50 @@
-from pytube import YouTube
 import os
 import streamlit as st
+from pytube import YouTube
 import base64
 
+# Dynamically get the user's download directory
+user_home = os.path.expanduser("~")
+download_folder = os.path.join(user_home, "Downloads")
 
+# Default audio file name
+default_audio_name = "download.mp3"
 
-def downloder(youtube_url, audio_name=None,name="download.mp3"):
-    audio = YouTube(youtube_url).streams.get_audio_only()
+def download_audio(youtube_url, audio_name=None):
+    yt = YouTube(youtube_url)
+    audio = yt.streams.get_audio_only()
+
     if audio_name:
-        name  = audio_name
-        if os.path.exists(r"C:/Users/Acer/Desktop/VICTOR2023/down/download.mp3"):
-            file_ = True
-        else:
-            file_ = False
-        
-        return audio.download("down",filename=name), file_
+        filename = audio_name
     else:
-        name = name
-    
-        if os.path.exists(r"C:/Users/Acer/Desktop/VICTOR2023/down/download.mp3"):
-            file_ = True
-        else:
-            file_ = False
-    
-        return audio.download("down",filename=name), file_
+        filename = default_audio_name
 
+    downloaded_file_path = audio.download(download_folder, filename=filename)
+    return downloaded_file_path
 
-   
-
-# C:\Users\Acer\Desktop\VICTOR2023\youtube_downloader.py
-
-# downloder("https://www.youtube.com/watch?v=mqSQvoinDE4")
 def main():
-    st.title("Vtube music Downloader")
-    url = st.text_input("Enter Youtube's video link or url", placeholder="https://www.youtube.com/watch?v=mqSQvoinDE4")
-    file_name = r'C:\Users\Acer\Desktop\VICTOR2023\down\download.mp3'
+    st.title("Vtube Music Downloader")
+    url = st.text_input("Enter YouTube video link or URL", placeholder="https://www.youtube.com/watch?v=mqSQvoinDE4")
+
     if "youtube" in url:
-        st.text(url)
-        _, file_ = downloder(url)
-        if file_:
-            
-            
-            with open(file_name, "rb") as audio_file:
-                audio_data = audio_file.read()
-                audio_bytes = base64.b64encode(audio_data).decode("utf-8")
-
-            st.audio(audio_data, format="audio/mp3")
-            st.download_button("Download", data=audio_data, file_name="download.mp3")
-        else:
-            "nope, try again"
+        # Ask for permission to download to download folder
+        permission = st.checkbox(f"Do you give permission to download the audio file to {download_folder}?")
         
+        if permission:
+            try:
+                downloaded_file_path = download_audio(url)
 
+                with open(downloaded_file_path, "rb") as audio_file:
+                    audio_data = audio_file.read()
+                    audio_bytes = base64.b64encode(audio_data).decode("utf-8")
 
+                st.audio(audio_data, format="audio/mp3")
+                st.download_button("Download", data=audio_data, file_name=default_audio_name)
+            except Exception as e:
+                st.error("An error occurred during download or playback.")
+                st.error(str(e))
+        else:
+            st.warning("You did not provide permission to download the file to your Downloads directory.")
 
 if __name__ == "__main__":
     main()
